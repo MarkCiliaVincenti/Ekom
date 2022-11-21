43f75ef99e85167e7f81a093e96d045aeca820d0
Garðar Þorsteinsson <gardar@vettvangur.is>
2022-11-21 16:16:19 +0000

    lowercase

--- Ekom/Models/PropertyValue.cs
+++ Ekom/Models/PropertyValue.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 
@@ -5,8 +6,11 @@ namespace Ekom.Models
 {
     public class PropertyValue
     {
+        [JsonProperty("dtdGuid")]
         public Guid DtdGuid { get; set; }
+        [JsonProperty("values")]
         public IDictionary<string, object> Values = new Dictionary<string, object>();
+        [JsonProperty("type")]
         public string Type {get; set;}
     }
 }
